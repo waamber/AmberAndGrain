4c0e669d5af59f038c48d99323d4a7406f846cab
waamber <amberastuart@aol.com>
2018-04-04 12:40:45 -0500

    post to batches and recipes

---
+++ AmberAndGraing/Services/BatchRepository.cs
@@ -0,0 +1,26 @@
+﻿using AmberAndGraing.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AmberAndGraing.Services
+{
+	public class BatchRepository
+	{
+		public bool Create(int recipeId, string cooker)
+		{
+			using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["AmberAndGrain"].ConnectionString))
+			{
+				db.Open();
+				var numberCreated = db.Execute(@"insert into batches(RecipeId, Cooker)
+												values(@RecipeId, @Cooker)", new { recipeId, cooker });
+
+				return numberCreated == 1;
+			}
+		}
+	}
+}
--- AmberAndGraing/Services/RecipeRepository.cs
+++ AmberAndGraing/Services/RecipeRepository.cs
@@ -1,16 +1,41 @@
-﻿using AmberAndGraing.Models;
+﻿
+using AmberAndGraing.Models;
+using Dapper;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
-namespace AmberAndGraing.Services
+namespace AmberAndGrain.Services
 {
 	public class RecipeRepository
 	{
 		public bool Create(RecipeDto recipe)
 		{
+			using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["AmberAndGrain"].ConnectionString))
+			{
+				db.Open();
 
+				var numberCreated = db.Execute(@"INSERT INTO [dbo].[Recipes]
+                                               ([Name]
+                                               ,[PercentWheat]
+                                               ,[PercentCorn]
+                                               ,[BarrelAge]
+                                               ,[BarrelMaterial]
+                                               ,[Creator])
+                                         VALUES
+                                               (@Name
+                                               ,@PercentWheat
+                                               ,@PercentCorn
+                                               ,@BarrelAge
+                                               ,@BarrelMaterial
+                                               ,@Creator)", recipe);
+
+				return numberCreated == 1;
+
+			}
 		}
 	}
 }
