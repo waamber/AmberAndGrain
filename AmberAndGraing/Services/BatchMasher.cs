bde776a0481123c4cc9b085e3abfa47adb986b6a
waamber <amberastuart@aol.com>
2018-04-07 12:39:16 -0500

    refactored errors

---
+++ AmberAndGraing/Services/BatchMasher.cs
@@ -0,0 +1,44 @@
+﻿using AmberAndGraing.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace AmberAndGraing.Services
+{
+	public class BatchMasher
+	{
+		public UpdateStatusResults MashBatch(int batchId)
+		{
+			var repository = new BatchRepository();
+			Batch batch;
+
+			try
+			{
+				batch = repository.Get(batchId);
+			}
+			catch (SqlException)
+			{
+				return UpdateStatusResults.Unsuccessful;
+			}
+			catch (Exception ex)
+			{
+
+				return UpdateStatusResults.NotFound;
+			}
+
+			if (batch.Status == BatchStatus.Created)
+			{
+				batch.Status = BatchStatus.Mashed;
+				var result = repository.Update(batch);
+
+				return result ? UpdateStatusResults.Success
+							  : UpdateStatusResults.Unsuccessful;
+			}
+
+			return UpdateStatusResults.ValidationFailure;
+		}
+	}
+}
